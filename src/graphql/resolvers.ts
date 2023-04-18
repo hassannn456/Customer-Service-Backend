81f465da012880426d2702946d2b51637c9d0175
Hassan Chaudhary <hassanfzl11@gmail.com>
2023-04-19 01:50:28 +0500

    Refactored the app flow

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,18 +1,20 @@
 import { authResolvers } from "./resolvers-helpers/auth";
 import { payeesResolvers } from "./resolvers-helpers/bankpayees";
+import { customerResolvers } from "./resolvers-helpers/customer";
 // import { customersResolvers } from "./resolvers-helpers/customers";
 // import { employeeResolvers } from "./resolvers-helpers/employee";
 
 const resolvers = {
   Mutation: {
     ...authResolvers.Mutation,
-    ...payeesResolvers.Mutation
+    ...payeesResolvers.Mutation,
     // ...customersResolvers.Mutation,
     // ...employeeResolvers.Mutation
   },
   Query: {
     ...authResolvers.Query,
-    ...payeesResolvers.Query
+    ...payeesResolvers.Query,
+    ...customerResolvers.Query
     // ...customersResolvers.Query,
   }
 };
