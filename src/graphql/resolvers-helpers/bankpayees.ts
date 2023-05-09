e4c2e34c1bbd0001639948e4b42d1d33fc57b5ec
Hassan Chaudhary <hassanfzl11@gmail.com>
2023-05-09 20:18:41 +0500

    PAyee function updated

--- src/graphql/resolvers-helpers/bankpayees.ts
+++ src/graphql/resolvers-helpers/bankpayees.ts
@@ -3,7 +3,8 @@ import { GraphQLError } from "graphql";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { Payees } from "../../entity/payees";
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import fetch from "node-fetch";
+import { Admin_users } from "../../entity/admin_users";
+import { bank_info } from "../../entity/bank_info";
 
 export const payeesResolvers = {
   Mutation: {
@@ -33,20 +34,23 @@ export const payeesResolvers = {
         const signedUrl = await getSignedUrl(s3, command, {
           expiresIn: 30,
         });
-
-        return { response: signedUrl };
+          return { response: signedUrl};
       } catch (err) {
         return { response: "Could not fetch image." };
       }
     },
     approveBankBook: async (
       _: any,
-      arg: { id: number }
+      arg: { id: number, remark: string },
+      context : { user : {id: any, roles: any}}
     ): Promise<{ response: Boolean } | undefined> => {
       try {
         const payee = await Payees.findOneBy({ id: arg.id });
+        const payeeUpdater = await Admin_users.findOneBy({ id: context.user.id });
         payee.active = true;
         payee.updated_at = new Date();
+        payee.updated_by = payeeUpdater.username
+        payee.remark = arg.remark
         if (await Payees.save(payee)) {
           return { response: true };
         }
@@ -59,16 +63,20 @@ export const payeesResolvers = {
     },
     rejectBankBook: async (
       _: any,
-      arg: { id: number }
+      arg: { id: number, remark: string },
+      context : { user : {id: any, roles: any}}
     ): Promise<{ response: Boolean } | undefined> => {
       try {
         const payee = await Payees.findOneBy({ id: arg.id });
+        const payeeUpdater = await Admin_users.findOneBy({ id: context.user.id });
         payee.account_name = null;
         payee.account_number = null;
         payee.routing_number = null;
         payee.book_image = null;
         payee.active = false;
         payee.updated_at = new Date();
+        payee.updated_by = payeeUpdater.username
+        payee.remark = arg.remark
         if (await Payees.save(payee)) {
           return { response: true };
         }
@@ -79,6 +87,19 @@ export const payeesResolvers = {
         });
       }
     },
+    getBank: async (
+      _: any,
+      arg: { routing_number: number},
+    ): Promise<{ response: string } | undefined> => {
+      try {
+        const payee = await bank_info.findOneBy({ routing_number: arg.routing_number });
+        return { response: payee.fi_name };
+      } catch (error) {
+        throw new GraphQLError("Bank not found.", {
+          extensions: { code: ApolloServerErrorCode.BAD_REQUEST },
+        });
+      }
+    }
   },
   Query: {
     getNewBankBooks: async (_: any, arg: Payees) => {
